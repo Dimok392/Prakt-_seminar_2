ffe9c55520d13c9df26a36bff228395a89beb441
Пётр <dimok392@mail.ru>
2022-04-23 09:41:26 +0300

    dz

---
+++ Example/example001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("трёхзначное число");
+int num = new Random().Next(100,1000); //int.Parse(Console.ReadLine());
+Console.WriteLine(num);
+int num2 = num/100;
+int num3 = num%10;
+int num4 = num2*10 + num3;
+Console.Write($"после удаления средней цыфры получил {num4}");
+
---
+++ Example/example005/Program.cs
@@ -0,0 +1,12 @@
+﻿int dni = 6;//new Random().Next(1,8); //Случайный отрезок от 1,,,9
+Console.Write($"{dni} день недели");
+int vih1 = 6;
+int vih = 7;
+if (dni==6 | dni==7)
+{
+    Console.Write($" это выходной");
+}
+else
+{
+    Console.Write($" это будний день");
+}
